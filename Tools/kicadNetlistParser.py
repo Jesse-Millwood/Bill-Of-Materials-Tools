78434dea575f46e5265a94506d5fc3accf747640
Jesse Millwood <jesse.c.millwood@gmail.com>
2015-03-31 09:17:06 -0400

    Reorganized. Got rid of old implementations. Fixed kicadnetlistparser

--- Tools/kicadNetlistParser.py
+++ Tools/kicadNetlistParser.py
@@ -7,81 +7,47 @@ Script to parse kicad netlists
 # pylint:disable = C0103
 
 from BOMparts import BOMpart
+import pyparsing
 
-class kicadComponent(object):
-    '''
-    Class for keeping Kicad component attributes
-    grouped together
-
-    *DEPRECIATED*
-    '''
-    def __init__(self):
-        self.ref = ''
-        self.value = ''
-        self.libsource = ''
-        self.footprint = ''
-        self.fields = {}
-
-def extractFieldDict(string):
-    '''
-    Convert the attributes in the feild to a
-    dictionary so it is searchable
-    '''
-    string = string.strip().replace('name ', '')
-    string = string.replace('field ', '')
-    string = string.replace('"', '')
-#    print (string)
-    fieldList = string.split(' ')
-    if len(fieldList) > 2:
-        fieldList[0] = ' '.join(fieldList[0:-1])
-        fieldList[1] = fieldList[-1]
-#    print (fieldList)
-    fieldDict = {fieldList[0]:fieldList[1]}
-#    print (fieldDict)
-    return(fieldDict)
         
+
 def extractKiCADComponents(filename):
     '''
-    Parses Kicad netlist file and extracts componets
+    Load a KiCAD netlist file and extract the part information
     '''
-    openparenFLAG = False
-    openParenCount = 0
-    closeParenCount = 0
-    compCount = 0
+    data = []
+    netlistComponents = []
     components = []
-    filelines = []
-    with open(filename) as f:
-        filelines = f.readlines()
-
     
-    for line in filelines:
-        if 'comp ' in line and openparenFLAG == False:
-            openparenFLAG = True
-            compCount +=1
-            components.append(BOMpart())
-        if openparenFLAG == True:
-            openParenCount += line.count('(')
-            closeParenCount += line.count(')')
-            line = line.replace('(', '')
-            line = line.replace(')', '')
-            line = line.replace('\n', '')
-
-            if 'ref' in line:
-                components[compCount-1].ref = line[line.find('ref')+len('ref')+1:]
-            if 'value' in line:
-                components[compCount-1].evalue = line[line.find('value')+len('value')+1:]
-            if 'footprint' in line:
-                components[compCount-1].footprint = line[line.find('footprint')+len('footprint')+1:]
-            if 'field ' in line:
-                components[compCount-1].attributes.update(extractFieldDict(line))
-            if 'libsource' in line:
-                components[compCount-1].library = line[line.find('libsource')+len('libsource')+1:]
-            if openParenCount == closeParenCount:
-                openparenFLAG = False
-
-        
+    # Read file as one long string
+    with open(filename, 'r') as f:
+        data = f.read().replace('\n', '')
+    # Parse the nested lists
+    parseddata = pyparsing.OneOrMore(pyparsing.nestedExpr()).parseString(data)
+    for section in parseddata[0]:
+        if 'components' in section[0]:
+            netlistComponents = section
+    for i, comp in enumerate(netlistComponents):
+        # Create new part
+        components.append(BOMpart())
+        for attribute in comp:
+            if 'ref' in attribute[0]:
+                components[i].ref = attribute[1]
+            elif 'value' in attribute[0]:
+                components[i].evalue = attribute[1]
+            elif 'footprint' in attribute[0]:
+                components[i].footprint = attribute[1]
+            elif 'fields' in attribute[0]:
+                for field in attribute[1:]:
+                    print('-'*10)
+                    print(field)
+                    print(field[1][1])
+                    print(field[2])
+                    components[i].attributes[field[1][1]] = field[2]
+            elif 'libsource' in attribute[0]:
+                try:
+                    components[i].library = attribute[1][1]
+                except NameError:
+                    print("libsource: {}".format(comp))
+                #components[i]. = attribute[2][1]
     return components
-
-
-
-
