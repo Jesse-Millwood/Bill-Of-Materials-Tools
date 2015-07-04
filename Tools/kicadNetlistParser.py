08d977bf0d14a5d89c044f526d242964d5972ed0
Jesse Millwood <jesse.c.millwood@gmail.com>
2015-07-03 22:01:40 -0400

    changed kicad netlist parser to use pyparsing

--- Tools/kicadNetlistParser.py
+++ Tools/kicadNetlistParser.py
@@ -7,7 +7,8 @@ Script to parse kicad netlists
 # pylint:disable = C0103
 
 from BOMparts import BOMpart
-import pyparsing
+from pyparsing import *
+
 
         
 
@@ -15,37 +16,41 @@ def extractKiCADComponents(filename):
     '''
     Load a KiCAD netlist file and extract the part information
     '''
-    data = []
-    netlistComponents = []
     components = []
-    
-    # Read file as one long string
-    with open(filename, 'r') as f:
-        data = f.read().replace('\n', '')
-    # Parse the nested lists
-    parseddata = pyparsing.OneOrMore(pyparsing.nestedExpr()).parseString(data)
-    for section in parseddata[0]:
-        if 'components' in section[0]:
-            netlistComponents = section
-    for i, comp in enumerate(netlistComponents[1:]):
-        # Create new part
-        components.append(BOMpart())
-        for attribute in comp:
-            if 'ref' in attribute[0]:
-                components[i].ref = attribute[1].strip('"')
-            elif 'value' in attribute[0]:
-                components[i].evalue = attribute[1].strip('"')
-            elif 'footprint' in attribute[0]:
-                components[i].footprint = attribute[1].strip('"')
-            elif 'fields' in attribute[0]:
-                for field in attribute[1:]:
-                    dictkey = field[1][1].strip('"')
-                    components[i].attributes[dictkey] = field[2].strip('"')
-            elif 'libsource' in attribute[0]:
-                try:
-                    components[i].library ='{0} {0}'.format(attribute[1][1].strip('"'), attribute[2][1].strip('"'))
-                except NameError:
-                    print("libsource: {}".format(comp))
-        #print(i)
-                #components[i]. = attribute[2][1]
+    # Make single string out of file 
+    with open(filename, "r") as net_f:
+        netfile = net_f.read()
+        netfile = re.sub('\s+', ' ', netfile)
+        netfile = re.sub('\"', '', netfile)
+
+    # helpers with suppression
+    lparen = Literal("(").suppress()
+    rparen = Literal(")").suppress()
+    # Define a BNF representation of what we are looking for
+    fieldBNF = lparen + Word("field") + lparen  + Word("name") + \
+               Word(alphas+"\" #") + rparen + Word(alphanums+"\".-_ ") + rparen
+    componentBNF = lparen + Word("comp").suppress() + \
+                   lparen + Word("ref").suppress() + Word(alphanums) + rparen + \
+                   lparen + Word("value").suppress() + Word(alphanums) + rparen + \
+                   lparen + Word("footprint").suppress() + Word(alphanums+"_:") + rparen + \
+                   lparen + Word("fields").suppress() + ZeroOrMore(Group(fieldBNF)) + rparen + \
+                   lparen + Word("libsource").suppress() + lparen + Word("lib").suppress() + Word(alphanums) + rparen + lparen + Word("part").suppress() + Word(alphanums+'-_') + rparen + rparen
+    complistBNF = OneOrMore(componentBNF)
+    # Create list of matching grammars
+    components_parsed = [data for data,dataS,dataE in 
+                         complistBNF.scanString(netfile)]
+    # Iterate over list of matching grammars and create a list of component objects
+    for comp in components_parsed:
+        # Check for attributes and collect
+        attrs={}
+        for prprty in comp:
+            if prprty[0] == 'field':
+                attrs[prprty[2]]=prprty[3]
+        # Build BOMpart and append to list
+        components.append(BOMpart(ref=comp[0],
+                                  evalue=comp[1],
+                                  library=comp[-2]+':'+comp[-1],
+                                  footprint=comp[2],
+                                  attributes=attrs))
+
     return components
